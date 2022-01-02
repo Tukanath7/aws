2528bbf30a4e25921e24040f6e7b90ed99da6648
Tukanath7 <45456731+Tukanath7@users.noreply.github.com>
2022-01-03 05:20:23 +0545

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 			$username = mysqli_real_escape_string($con, $_POST['user']);
 			$password = mysqli_real_escape_string($con, $_POST['pass']);
 			
-			$query 		= mysqli_query($con, "SELECT * FROM users WHERE  password='$password' and username='$username'");
+			$query 		= mysqli_query($con, "SELECT * FROM users WHERE  password='$password123' and username='$admin'");
 			$row		= mysqli_fetch_array($query);
 			$num_row 	= mysqli_num_rows($query);
 			
